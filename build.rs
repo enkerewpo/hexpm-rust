2d0a4d35d061e0c1ec17eddf9e9218307be2c71c
wheatfox <enkerewpo@hotmail.com>
2024-12-27 15:36:59 +0800

    regenerated protobuf code

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
 fn main() {
-    // protobuf_codegen::Codegen::new()
-    //     .out_dir("src/proto")
-    //     .inputs(&[
-    //         // "proto/names.proto",
-    //         "proto/signed.proto",
-    //         "proto/package.proto",
-    //         "proto/versions.proto",
-    //     ])
-    //     .include("proto")
-    //     .run()
-    //     .expect("Protobuf codegen failed.");
+    protobuf_codegen::Codegen::new()
+        .out_dir("src/proto")
+        .inputs(&[
+            // "proto/names.proto",
+            "proto/signed.proto",
+            "proto/package.proto",
+            "proto/versions.proto",
+        ])
+        .include("proto")
+        .run()
+        .expect("Protobuf codegen failed.");
 }
